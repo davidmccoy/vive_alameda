47ee74a39668de0ff871639a8c7d39842c029669
davidmccoy <david.mccoy@gmail.com>
2016-07-14 16:27:55 -0500

    added project info and language options. added photos

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,1 +1,23 @@
-// This is where it all goes :)
+// Click event to change project info language
+
+window.onload = function() {
+  var spans = document.getElementsByClassName('language-option');
+  var clickEvent = function() {
+    if (!this.classList.contains('active') && this.id === "espanol") {
+      this.classList.add('active');
+      document.getElementById('info-espanol').classList.add('active');
+
+      document.getElementById('english').classList.remove('active');
+      document.getElementById('info-english').classList.remove('active');
+    } else if (!this.classList.contains('active') && this.id === "english") {
+      this.classList.add('active');
+      document.getElementById('info-english').classList.add('active')
+
+      document.getElementById('espanol').classList.remove('active');
+      document.getElementById('info-espanol').classList.remove('active');
+    }
+  }
+  for(i = 0; i < spans.length; i++) {
+    spans[i].addEventListener('click', clickEvent)
+  }
+}
